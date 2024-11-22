22a00899e1178bd52b3e50df2adef9bbf1ee7c13
ico-mode31 <hanzysetnuef@gmail.com>
2024-11-22 10:56:34 +0800

    ico

---
+++ icocincout/icocincout/icocincout.cpp
@@ -0,0 +1,16 @@
+// icocincout.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int age;
+    cout << "What is your Age: ";
+        cin >> age;
+
+    cout << "Here is your Age: " << age;
+
+    return 0;
+}
